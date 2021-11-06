b7a5b422ec7063423711f2f52d820d667169cd6e
Frans Junior Wangsa <90809119+malugan@users.noreply.github.com>
2021-11-06 11:13:06 +0800

    Add files via upload

---
+++ rumusLuasPersegi.java
@@ -0,0 +1,10 @@
+public class rumusLuasPersegi{
+	
+	void hitung(double sisi){
+		System.out.println("Luas Persegi = " +(sisi*sisi) +"cm");
+	}
+	
+	void tampil(){
+		System.out.println("Rumus Luas Persegi : sisi*sisi");
+	}
+}
